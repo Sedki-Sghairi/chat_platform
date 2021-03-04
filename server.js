554e03b814b7f23094ac1de991dbdba2dc700fe2
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-03-04 21:07:30 +0400

    peerJS initial setup

--- server.js
+++ server.js
@@ -18,9 +18,13 @@ app.get('/:room', (req, res) => {
 
 io.on('connection', (socket) => {
 	socket.on('join-room', (roomId, userId) => {
-		console.log('roomId: ', roomId);
-		console.log('userId: ', userId);
+		socket.join(roomId);
+		socket.to(roomId).broadcast.emit('user-connected', userId);
 	});
 });
+
+const { PeerServer } = require('peer');
+
+const peerServer = PeerServer({ port: 9000, path: '/myapp' });
 
 server.listen(3000);
